cd5f6658c85147483652f002effb04b0e3db71c9
Ampuh <ampuh.rakan@gmail.com>
2022-06-15 15:59:07 +0700

    FPG-33

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Footer from "../components/Footer";
 import AddProductSuccess from "./addProductSuccess";
 import Admin from "./admin";
-import Admin2 from "./admin2";
 export default function Home() {
   return (
     <div>
@@ -11,7 +10,6 @@ export default function Home() {
         <link rel="icon" href="/pro.ico" />
       </Head>
       <Admin />
-      <Admin2 />
       <AddProductSuccess />
       {/* <Footer /> */}
     </div>
