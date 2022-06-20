311353a1eb0b15b6b9f7706018668f25bf9080d6
Ampuh <ampuh.rakan@gmail.com>
2022-06-20 15:46:49 +0700

    a

--- pages/profile/profile.jsx
+++ pages/profile/profile.jsx
@@ -28,38 +28,71 @@ const Profile = () => {
   const { name, email, gender, birthdate } = useUser();
   return (
     // <Box boxShadow="2xl" p="6" rounded="md" bg="white">
-    <div className="flex col shadow-purple-200 shadow-2xl h-5/6 rounded-lg mt-10 py-20 mx-48 justify-center ">
-      <div>
-        <div className=" bg-red-400 pt-10 ml-11 text-4xl font-bold">Profil</div>
-        <Divider className="ml-11" />
+    <div classname="flex flex-row w-screen">
+      <div className="shadow-purple-200 bg-orange-200 shadow-2xl rounded-lg mt-10 w-48">
+        <div className=" bg-red-400 pt-10 ml-11 text-4xl font-bold">
+          Jane Doe
+        </div>
+        {/* <Divider className="ml-11" /> */}
         <div className="flex">
-          <img
-            className="flex items-center ml-11 mt-10 bg-yellow-400 h-[300px]"
-            src={"/addProductSuccess.svg"}
-          />
-
           <div>
             <div className="flex mt-10">
-              <div className="profileTag">Username</div>
-              <div className="profileDesc ml-16">{name}</div>
+              <div className="profileTag">Profil</div>
             </div>
             <div className="flex">
-              <div className="profileTag">Nama Lengkap</div>
-              <div className="profileDesc ml-7">Ampuh Rakan</div>
+              <div className="profileTag">Proses Pemesanan</div>
             </div>
             <div className="flex">
-              <div className="profileTag">Gender</div>
-              <div className="profileDesc ml-[85px]">Pria</div>
+              <div className="profileTag">Metode Pembayaran</div>
             </div>
             <div className="flex">
-              <div className="profileTag">E-mail</div>
-              <div className="profileDesc ml-[93px]">
-                rakan.ganteng@gmail.com
+              <div className="profileTag">Alamat Pengiriman</div>
+            </div>
+            <div className="flex">
+              <div className="profileTag">Wishlist</div>
+            </div>
+            <div className="flex">
+              <div className="profileTag">Pesan Bantuan</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex bg-green-300 shadow-purple-200 shadow-2xl h-5/6 rounded-lg mt-10   ">
+        <div>
+          <div className=" bg-red-400 pt-10 ml-11 text-4xl font-bold">
+            Profil
+          </div>
+          {/* <Divider className="ml-11" /> */}
+          <div className="flex">
+            <img
+              className="flex items-center ml-11 mt-10 bg-yellow-400 h-[300px]"
+              src={"/addProductSuccess.svg"}
+            />
+
+            <div>
+              <div className="flex mt-10">
+                <div className="profileTag">Username</div>
+                <div className="profileDesc ml-16">{name}</div>
               </div>
-            </div>
-            <div className="flex">
-              <div className="profileTag">Umur</div>
-              <div className="profileDesc ml-[97px]">25</div>
+              <div className="flex">
+                <div className="profileTag">Nama Lengkap</div>
+                <div className="profileDesc ml-7">Ampuh Rakan</div>
+              </div>
+              <div className="flex">
+                <div className="profileTag">Gender</div>
+                <div className="profileDesc ml-[85px]">Pria</div>
+              </div>
+              <div className="flex">
+                <div className="profileTag">E-mail</div>
+                <div className="profileDesc ml-[93px]">
+                  rakan.ganteng@gmail.com
+                </div>
+              </div>
+              <div className="flex">
+                <div className="profileTag">Umur</div>
+                <div className="profileDesc ml-[97px]">25</div>
+              </div>
             </div>
           </div>
         </div>
