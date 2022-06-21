2d604740b7558815772e5b0accbb720c36f850e0
AndhikaPrasetyo <andhikapras15@gmail.com>
2022-06-21 14:32:34 +0700

    development

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,12 @@
-import Head from "next/head";
-import Footer from "../components/Footer";
-import AddProductSuccess from "./addProductSuccess";
-import Admin from "./admin";
-import Admin2 from "./admin2";
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css' 
+
+
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Footer</title>
-        <link rel="icon" href="/pro.ico" />
-      </Head>
-      <Admin />
-      <Admin2 />
-      <AddProductSuccess />
-      {/* <Footer /> */}
+      ini Homenya
     </div>
-  );
+  )
 }
