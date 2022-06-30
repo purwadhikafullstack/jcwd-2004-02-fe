cf479183592add24d8bf22d8b957740891715616
Ampuh <ampuh.rakan@gmail.com>
2022-06-30 09:17:24 +0700

    fpg2-22

--- pages/prescriptionBerhasil.js
+++ pages/prescriptionBerhasil.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { useRouter } from "next/router";
 import Navbar from "../components/navbar";
 import Footer from "../components/Footer";
