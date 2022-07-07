e24f3be1a7360be80cb37e37f082b220e0d23586
AndhikaPrasetyo <andhikapras15@gmail.com>
2022-07-07 16:36:36 +0700

    Resolved name

--- pages/prescription.js
+++ pages/prescription.js
@@ -1,7 +1,7 @@
 import React, { Component, useCallback, createRef } from "react";
 import { useEffect, useState } from "react";
 import { Divider, Box, Image, Button } from "@chakra-ui/react";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/navbar";
 import { API_URL } from "../helpers";
 import { useDropzone } from "react-dropzone";
 import { BsImage } from "react-icons/bs";
