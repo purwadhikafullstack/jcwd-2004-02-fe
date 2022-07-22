ef1080c7ee6fae77795b52f91b9059ebf2a44c06
Ampuh <ampuh.rakan@gmail.com>
2022-07-22 17:19:16 +0700

    styling

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    NEXT_PUBLIC_API_URL: "http://jcwd200402api.purwadhikabootcamp.com",
+    NEXT_PUBLIC_API_URL: "http://localhost:5000",
   },
   images: {
     domains: ["localhost"],
