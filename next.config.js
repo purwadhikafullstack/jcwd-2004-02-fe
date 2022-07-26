1d574fe351356efade6eb1c407a29d878ceb3410
AndhikaPrasetyo <andhikapras15@gmail.com>
2022-07-26 23:49:07 +0700

    bug-3-andhika

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    NEXT_PUBLIC_API_URL: "http://localhost:5000",
-    // NEXT_PUBLIC_API_URL: "https://jcwd200402api.purwadhikabootcamp.com",
+    // NEXT_PUBLIC_API_URL: "http://localhost:5000",
+    NEXT_PUBLIC_API_URL: "https://jcwd200402api.purwadhikabootcamp.com",
   },
   images: {
     domains: ["localhost", "jcwd200402api.purwadhikabootcamp.com"],
