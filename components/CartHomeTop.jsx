1a684f7d135e1571f224eca6aabac1d92397d253
grisheldas <grisheldaathallahsilviyana@gmail.com>
2022-07-26 15:20:02 +0700

    fe

--- components/CartHomeTop.jsx
+++ components/CartHomeTop.jsx
@@ -19,17 +19,10 @@ function CardHomeTop({ img, name, price, unit }) {
         <div className="lg:h-[113px] h-[76px] bg-slate-200 overflow-hidden relative">
           <Image src={img} layout="fill" objectFit="cover"></Image>
         </div>
-        <div className="h-[34px] lg:h-[30px] mt-2 text-xs lg:text-sm font-bold text-purple-800 truncate">
+        <div className="h-[50px] mb-2 lg:h-[65px] lg:mb-2 mt-2 text-xs lg:text-sm font-bold text-purple-800 text-ellipsis overflow-hidden">
           {name}
         </div>
-        <div className="flex items-center tracking-wide h-[21px] mb-[4px] lg:h-[24px] lg:mb-[7px] mt-[8px] text-xs">
-          <div className="border-2 border-orange-300 text-orange-300 rounded-md px-1 font-semibold">
-            17%
-          </div>
-          <div className="text-xs pl-2 line-through decoration-1 decoration-slate-600 text-slate-500">
-            {rupiah(65000)}
-          </div>
-        </div>
+
         <div className="grid grid-flow-row-dense grid-cols-2 grid-rows-2 lg:grid-cols-3 text-xs text-purple-800">
           <div className="col-span-2 font-bold">{rupiah(price)}</div>
           <div className="text-xs lg:text-xs font-semibold lg:text-right">
